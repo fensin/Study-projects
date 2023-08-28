799498dac03103ef82e8e596d5acd84a59bfcba5
Ilia <qwertyfen@gmail.com>
2023-08-29 02:01:33 +0700

    feat: add modals

--- library/main.js
+++ library/main.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
+
   document.getElementById("burger").addEventListener("click",function()
   {
     document.querySelector(".header").classList.toggle("open")
+  })
+
+  document.getElementById("profile").addEventListener("click",function()
+  {
+    document.querySelector(".ModalProfile_noAuth").classList.toggle("noAuth")
   })
 })
