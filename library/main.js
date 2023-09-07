3086bdfc7809decf1f279ea2fa309e6ce037400b
Ilia <qwertyfen@gmail.com>
2023-09-07 09:58:59 +0700

    feat: add events for log and reg modals

--- library/main.js
+++ library/main.js
@@ -2,30 +2,26 @@ const icon = document.querySelector('.header__profile-icon');
 const burger = document.querySelector('#burger');
 const modalProfile = document.querySelector('.ModalProfile_noAuth');
 const header = document.querySelector('.header');
-const substrate = document.querySelectorAll('.substrate');
+const burgerBack = document.querySelector('.burger_back');
 const burgerItem = document.querySelectorAll('.navigation-item');
 const burgerMenu = document.querySelector('.header__navigation');
-
-//Backgroung changing
-function subActivate () {               
-  substrate.forEach(function (item) {
-    item.classList.toggle('active');
-  }
-)}
 
 //Burger menu open and close function
 function burgerToggle() {
   header.classList.toggle('open');
-  modalProfile.classList.remove('noAuth');
-  subActivate();
+  modalProfile.classList.remove('profActive');
+  backActivate();
 } 
 
 burger.addEventListener('click', burgerToggle);
 
+//Backgroung changing
+function backActivate () {               
+  burgerBack.classList.toggle('active');
+}
+
 //Background click event
-substrate.forEach(function (item) {
-  item.addEventListener('click', burgerToggle);
-})
+burgerBack.addEventListener('click', burgerToggle);
 
 //Click on burger items
 burgerItem.forEach (function (item) {
@@ -33,11 +29,64 @@ burgerItem.forEach (function (item) {
 })
 
 //Click on profile icon
-icon.addEventListener('click', () => {
-  modalProfile.classList.toggle('noAuth');
+function profileToggle () {
+  modalProfile.classList.toggle('profActive');
   header.classList.remove('open');
-  substrate.forEach(function (item) {
-    item.classList.remove('active');
+  burgerBack.classList.remove('active');
+}
+
+icon.addEventListener('click', profileToggle)
+
+//LogIn modal
+const btnLogIn = document.querySelector('#logIn');
+const btnRegister = document.querySelector('#register');
+const modalWrap = document.querySelector('#modalWrap');
+const modalLog = document.querySelector('.log');
+const modalReg = document.querySelector('.reg');
+const modalCross = document.querySelectorAll('.modal__cross');
+const lnkLogIn = document.querySelector('#lnkLog');
+const lnkReg = document.querySelector('#lnkReg');
+
+btnLogIn.addEventListener('click', () => {
+  profileToggle();
+  modalWrap.classList.toggle('modal_hidden');
+  modalLog.classList.toggle('active');
+})
+
+function closeModals () {
+  modalWrap.classList.add('modal_hidden');
+  modalLog.classList.remove('active');
+  modalReg.classList.remove('active');
+}
+
+modalCross.forEach (function (item) {
+  item.addEventListener('click', () => {
+    closeModals()
   })
 })
 
+btnRegister.addEventListener('click', () => {
+  profileToggle();
+  modalWrap.classList.toggle('modal_hidden');
+  modalReg.classList.add('active');
+})
+
+function backClose (e) {
+  const click = e.composedPath().includes(modalLog);
+  const click2 = e.composedPath().includes(modalReg);
+  if (click == false && click2 == false) {
+    closeModals()
+  }
+}
+
+modalWrap.addEventListener('click', backClose);
+
+lnkReg.addEventListener('click', () => {
+  modalLog.classList.remove('active');
+  modalReg.classList.add('active');
+})
+
+lnkLogIn.addEventListener('click', () => {
+  modalReg.classList.remove('active');
+  modalLog.classList.add('active');
+})
