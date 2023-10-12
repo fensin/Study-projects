6700c49bd01e5cdbe93e625eb2eee7bc12145e8d
Ilia <qwertyfen@gmail.com>
2023-10-12 11:18:26 +0700

    refactor: code refactor with prettier

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,23 +1,23 @@
-'use strict'
+'use strict';
 
 const client_id = 'cDQviJlolRiyhIQgknk55Bi6vD_frBdFn-zfKNjol1Y';
 const container = document.querySelector('#gallery');
 const searchInput = document.querySelector('#search');
-const inputContainer = document.querySelector('.head__search')
+const inputContainer = document.querySelector('.head__search');
 const form = document.querySelector('form');
 
 let inputData = 'gundam';
 
 //Fetch from API
 async function getImgs(inputData) {
-  const url = `https://api.unsplash.com/search/photos?query=${inputData}&orientation=landscape&page=1&per_page=30&client_id=${client_id}`
+  const url = `https://api.unsplash.com/search/photos?query=${inputData}&orientation=landscape&page=1&per_page=30&client_id=${client_id}`;
 
   const response = await fetch(url);
   const data = await response.json();
   const results = data.results;
-  
+
   if (results.length > 0) {
-    showImgs(results);  
+    showImgs(results);
   } else {
     noImages();
   }
@@ -28,7 +28,7 @@ function showImgs(results) {
   container.classList.remove('noImg');
 
   container.innerHTML = '';
-  results.forEach(element => {
+  results.forEach((element) => {
     const img = document.createElement('img');
     const imgWrapper = document.createElement('div');
     imgWrapper.classList.add('imgWrapper');
@@ -63,19 +63,19 @@ searchBtn.addEventListener('click', () => {
   if (searchBtn.classList.contains('active')) {
     searchImgs();
   }
-})
+});
 
 //Click on cross icon
 eraseBtn.addEventListener('click', () => {
   searchInput.value = '';
   eraseBtn.classList.add('inactive');
-})
+});
 
 //Cross and search icon toggle
 searchInput.addEventListener('input', () => {
-
   //Check for only spaces in input
   const checkSpaces = searchInput.value.trim() == '';
+  console.log(checkSpaces);
   if (checkSpaces) {
     searchInput.value = '';
   }
@@ -87,10 +87,10 @@ searchInput.addEventListener('input', () => {
     eraseBtn.classList.add('inactive');
     searchBtn.classList.replace('active', 'inactive');
   }
-})
+});
 
 //Search enter press
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   searchImgs();
-})
+});
